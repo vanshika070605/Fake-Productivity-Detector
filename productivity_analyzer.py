19705569d4e1cdc8b3b8f1f22115bac114d57dd1
vanshika070605 <agrawalvanshika0706@gmail.com>
2026-06-12 13:32:43 +0530

    Update productivity_analyzer.py

--- productivity_analyzer.py
+++ productivity_analyzer.py
@@ -1,13 +1,16 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("activity.csv")
 
+df["Category"] = df["Category"].str.lower()
+
 total_time = df["Time_Minutes"].sum()
 
-productive_time = df[df["Category"] == "Productive"]["Time_Minutes"].sum()
 if total_time == 0:
     score = 0
 else:
+    productive_time = df[df["Category"] == "productive"]["Time_Minutes"].sum()
     score = (productive_time / total_time) * 100
 
 print("Productivity Score:", round(score, 2), "%")
@@ -18,13 +21,17 @@ elif score >= 50:
     print("Moderately Productive")
 else:
     print("Fake Productivity Detected")
-import matplotlib.pyplot as plt
 
+# Pie chart
 data = df.groupby("Category")["Time_Minutes"].sum()
 
 plt.pie(data, labels=data.index, autopct="%1.1f%%")
 plt.title("Productivity Analysis")
 plt.show()
+
+# Focus score
+focus_score = min(score + 10, 100)
+print("Focus Score:", round(focus_score, 2), "%")
 
 focus_score = ((productive_time / total_time) * 100) + 10
 
